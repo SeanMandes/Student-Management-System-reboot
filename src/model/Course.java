eaa268bb61e0dcf185051e6550d16641fdd9108a
Son Nguyen <son.nguyen4@ucalgary.ca>
2026-04-08 13:34:57 -0600

    initilize course objects

--- src/model/Course.java
+++ src/model/Course.java
@@ -1,5 +1,30 @@
 package model;
 
 public class Course {
+    private String course_name;
+    private String instructor;
+    private int id;
 
+    public Course(String course_name, String instructor) {
+        this.course_name = course_name;
+        this.instructor = instructor;
+    }
+
+    public Course(int id, String course_name, String instructor) {
+        this.id = id;
+        this.course_name = course_name;
+        this.instructor = instructor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCourseName() {
+        return course_name;
+    }
+
+    public String getInstructor() {
+        return instructor;
+    }
 }
