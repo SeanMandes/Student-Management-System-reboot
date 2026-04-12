95f5741c3f442257750d8cbd2b1b1360f5b70919
Son Nguyen <son.nguyen4@ucalgary.ca>
2026-04-11 22:58:22 -0600

    add enrollment datas

--- src/dao/EnrollmentDAO.java
+++ src/dao/EnrollmentDAO.java
@@ -1,5 +1,19 @@
 package dao;
 
+import model.Course;
+import model.Student;
+
 public class EnrollmentDAO {
+    public void enrollStudent(Student student, Course course) {
+
+    }
+
+    public void removeEnrollment() {
+
+    }
+
+    public void assignGrade() {
+
+    }
 
 }
--- src/model/Enrollment.java
+++ src/model/Enrollment.java
@@ -1,5 +1,32 @@
 package model;
 
 public class Enrollment {
+    private int courseId;
+    private int studentId;
+    private int enrollmentId;
+    private String grade;
+
+    public Enrollment(int courseId, int studentId, int enrollmentId, String grade) {
+        this.courseId = courseId;
+        this.studentId = studentId;
+        this.enrollmentId = enrollmentId;
+        this.grade = grade;
+    }
+
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public int getEnrollmentId() {
+        return enrollmentId;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
 
 }
